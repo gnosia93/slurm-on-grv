b8c4a30a32317c7a38cb7f75860f7d202df3ca83
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-25 12:45:12 +0900

    Update slc-output.tf

--- tf/slc-output.tf
+++ tf/slc-output.tf
@@ -1,3 +1,11 @@
+output "master" {
+  value = values(module.slc-mst)[0].public_ip
+}
+
+output "client" {
+  value = values(module.slc-mst)[1].public_ip
+}
+
 output "graviton-workers" {
   value = [for instance in module.slc-wg : instance.public_ip]
 }
