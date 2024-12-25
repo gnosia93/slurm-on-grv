c19b73680cb5a89c90628711a3f1c2bf73441f9c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-25 12:44:25 +0900

    Update slc-output.tf

--- tf/slc-output.tf
+++ tf/slc-output.tf
@@ -1,3 +1,7 @@
 output "graviton-workers" {
   value = [for instance in module.slc-wg : instance.public_ip]
 }
+
+output "nvidia-workers" {
+  value = [for instance in module.slc-wn : instance.public_ip]
+}
