ae82a9530f272c9e78b67e45c7b47a2a42faa3bf
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-01-02 19:03:38 +0900

    Update cifar10-vgg.py

--- ddp/cifar10-vgg.py
+++ ddp/cifar10-vgg.py
@@ -14,7 +14,6 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn 
 import torch.optim as optim
-import torch.multiprocessing as mp 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 from torchvision.datasets.cifar import CIFAR10
